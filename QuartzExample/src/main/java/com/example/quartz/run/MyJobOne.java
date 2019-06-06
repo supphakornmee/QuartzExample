587f9de0f1afeacb29bd10758d40e1a2757505ac
supphakornmee <supphakornmee@counterservice.co.th>
2019-06-06 11:44:42 +0700

    add log4j

--- QuartzExample/src/main/java/com/example/quartz/run/MyJobOne.java
+++ QuartzExample/src/main/java/com/example/quartz/run/MyJobOne.java
@@ -1,14 +1,26 @@
 package com.example.quartz.run;
 
+
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
 
 public class MyJobOne implements Job{
 
+	private static Logger LOGGER = LogManager.getLogger(MyJobOne.class);
+	
 	@Override
 	public void execute(JobExecutionContext arg0) throws JobExecutionException {
 		// TODO Auto-generated method stub
+		
+		LOGGER.info("Start MyJobOne.");
 		System.out.println("Start MyJobOne.");
 	}
 
--- QuartzExample/src/main/java/com/example/quartz/run/MyJobTwo.java
+++ QuartzExample/src/main/java/com/example/quartz/run/MyJobTwo.java
@@ -1,14 +1,18 @@
 package com.example.quartz.run;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
 public class MyJobTwo implements Job{
 
+	private static Logger LOGGER = LogManager.getLogger(MyJobTwo.class);
 	@Override
 	public void execute(JobExecutionContext arg0) throws JobExecutionException {
 		// TODO Auto-generated method stub
+		LOGGER.info("Start MyJobTwo.");
 		System.out.println("Start MyJobTwo.");
 		
 	}
